0e6ae0a712273baedbcc59a03fa0b6bfad3fbbd4
MegaEpicCoolDude <46431368+MegaEpicCoolDude@users.noreply.github.com>
2019-01-06 20:39:26 -0600

    Scanner
    Scans and reprints

--- This.java
+++ This.java
@@ -1,6 +1,10 @@
+import Scanner;
+
 class this {
     public static void main(String [args]) {
-         System.out.println("HELLO!\n What's up?");
+        System.out.println("HELLO!\n What's up?");
+        Scanner ah = new Scanner(System.in);
+        System.out.println(ah);
     }
 
 }
